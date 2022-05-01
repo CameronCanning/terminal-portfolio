a2d8e867a706a08dbac04d61e1b6e5d8e77354bb
Cameron Canning <cameroncanning.1@gmail.com>
2022-04-30 21:20:23 -0400

    proof of concept for auto typing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
-  const portfolioCommand = 'cd portfolio && ls';
-  const [command, setCommand] = useState()
-  useEffect(() => {
-    portfolioCommand.split('').forEach(element => {
-      console.log(element);
-    });
-  },[]); 
+	const [command, setCommand] = useState(
+		{
+			initial: 'cd portfolio && ls',
+			visible: ''
+		}
+	);
+	let interval;
+	useEffect(() => {
+		if (command.initial) { 
+			interval = setInterval(() => {
+				setCommand(prev => {
+					return {					
+						initial: prev.initial.substring(1),
+						visible: prev.visible + prev.initial[0]
+					}					
+				})
+				}, 100);
+			return () => clearTimeout(interval);
+		}
+		else {
+			console.log('run');
+		}	
+	}, [command]);
+	
+	
+	return (
+		<div className='terminal'>
+			<div className='terminal-line'>
+				<span className='user-color'>cameroncanning</span>
+				<span className='dir-color'>:~$</span>
+				<input 
+					id='terminal-input' 
+					autoComplete='false' 
+					autoFocus 
+					value={command.visible} 
+					onChange={(e) => { e.preventDefault() }} 
 
-  
-  return (
-    <div className='terminal'>
-      <code>
-        <span className='userColor'>cameroncanning</span><span className='dirColor'>:~$</span>{portfolioCommand}
-      </code>
-    </div>
-    
-  );
+					/>
+			</div>
+		</div>
+
+	);
 }
 
 export default App;
