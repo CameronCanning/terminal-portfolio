c29bd8ddc1ecc9edfcd73205bd1df279688844ec
Cameron Canning <cameroncanning.1@gmail.com>
2022-05-03 04:42:44 -0400

    initial commands to run automaticlly after the terminal "loads", commads object to define command names and their result

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import Terminal from './components/Terminal';
 
 function App() {
 	
-	const autoCommand = {
-		command: 'cd portfolio && ls',
-		directory: {
-		  path: 'portfolio'			
-		},
-		startDelay: 0,
-		endDelay: 0,
-		speed: 50,
-		callback: () => console.log('yo')
-	  }
-
 	return (		
-		<Terminal user={'cameroncanning'} auto={autoCommand}/>
+		<Terminal 
+			user={'cameroncanning'} 
+			/>
 	);
 }
 
--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,82 +1,147 @@
 import { useState, useEffect, useRef } from 'react';
 
-const Terminal = ({noHeader, user}) => {
-    const [auto, setCommand] = useState({command: '', output: <p>test</p>});
-    const [contentArray, setContentArray] = useState([]);
-    const [current, setCurrent] = useState({
-        command: '',
-        dir: '',
-        typing: false,
-        user: user
-    });
+const lsPortfolio = 
+    <span>
+        <span className='content-command'>about.txt</span>
+        <span className='content-command'>education.txt</span>
+        <span className='content-command'>skills.json</span>
+        <span className='content-command'>projects/</span>
+    </span>;
 
+const commands = {
+    'cd portfolio': { dir: '/portfolio' },
+    'ls': { output: ({dir}) => {
+        switch (dir) {
+            case '/portfolio':
+                return lsPortfolio      
+            case '/projects':
+                return <p>projects</p>
+
+        }
+    }}   
+}
+const Terminal = (props) => {
+    const [loadTime, setLoadTime] = useState(props.loadTime || 2100);  
+    const [sending, setSending] = useState(false);
+    const [auto, setAuto] = useState('');
+    const [contentArray, setContentArray] = useState([]);
+    const [editor, setEditor] = useState({});
+
+    const [initCommands, setInitCommands] = useState(['cd portfolio', 'ls', 'ls', 'ls']);
+    const notInitRender = useRef(false);
 	//auto type command on update
-	useEffect(() => {
-		if (auto.command) {
-			setTimeout(() => {   
-                setCommand(prev => {return { ...prev, command: prev.command.substring(1)}});       				
-                setCurrent(prev => {
+	useEffect(() => {   
+        if (auto) {
+            setTimeout(() => {   
+                setAuto(prev => prev.substring(1));       				
+                setEditor(prev => {
                     return {
                         ...prev,
-                        command: prev.command + auto.command[0]
+                        command: prev.command + auto[0]
                     }
                 })
-            }, 50)
-		}
-		else if (current.typing) {
-            console.log('run');
-            setCurrent(prev => {return {...prev, typing: false}});
-            setTimeout(() => {
-                nextContent()
-            }, 1000);
-		}
+            }, props.typingSpeed || 50)
+        }
+        else if (editor.typing && editor.command) {
+            setTimeout(() => executeCommand(), 500);
+            setEditor(prev => {return {...prev, typing: false}}); 
+        }
 	}, [auto]);
 
-    //intro animation
     useEffect(() => {
-        sendCommand('cd portfolio', null, 1500, '/portfolio');
-        sendCommand('ls', lsPortfolio, 3500);
-        sendCommand('ls', lsPortfolio, 7000);
-        sendCommand('ls', lsPortfolio, 10000);
-        
+        if (notInitRender.current && !sending){
+            console.log('sending rest');
+            sendCommand(initCommands.shift());
+        }
+        else {
+            notInitRender.current = true
+        }
+    }, [sending]);
+
+    //load effect then call initial command
+    useEffect(() => {
+        if (loadTime) {
+            console.log('loading...')
+            setTimeout(() => setLoadTime(0), loadTime);    
+        }
+        else if (initCommands.length > 0) {
+            console.log('sending first');
+            let newInitCommands = [...initCommands];
+            sendCommand(newInitCommands.shift());
+            setInitCommands(newInitCommands);
+        }
+
+    }, [loadTime]);
+
+    useEffect(() => {
+        setContentArray([]);
+        setEditor({
+            command: '',
+            dir: '',
+            typing: false,
+            user: props.user || 'user'
+        });
     }, []);
 
-    const sendCommand = (command, output, delay, dir) => {  
-        setTimeout(() => {
-            setCurrent(prev => {return {...prev, typing: true}});            
-            setCommand(prev => {return {...prev, command: command, output: output}});
-        }, delay); 
-
+    const sendCommand = (command, delay=1000) => { 
+        if (!editor.typing){
+            setTimeout(() => {
+                setEditor(prev => {return {...prev, typing: true}});    
+                setSending(true);
+                setAuto(command)        
+            }, delay)
+        }            
     }
-    const nextContent = () => {
+
+    const executeCommand = () => {
+        setSending(false);
+        let output;
+        if (commands[editor.command]) {
+            if (typeof commands[editor.command].output === 'function') {
+                output = commands[editor.command].output(editor);
+            }
+            else {
+                output = commands[editor.command].output;
+            }
+            setEditor(prev => { return {...prev, command: '', dir: commands[editor.command].dir || prev.dir}}); 
+        }
+        else {
+            output = <p>Command doesn't exist</p>
+            setEditor(prev => { return {...prev, command: '', dir: prev.dir}}); 
+        }
         //state -> strings
         setContentArray(prev => [...prev, {
-            user: `${user}`,
-            command: `${current.command}`,
-            dir: `${current.dir}`,
-            output: auto.output,
-            dead: true,
+            user: `${props.user}`,
+            command: `${editor.command}`,
+            dir: `${editor.dir}`,
+            output: output,
         }]);
-        setCurrent(prev => { return {...prev, command: '', dir: '/portfolio'}}); 
+        
     }
+
     return (
         <div className='terminal'>
-            {
-                <>
-                {!noHeader && <Header/>}
-                <div className='bottom-scroll'>
-                    <div className='terminal-content-wrapper'>
+            {!props.noHeader && <Header/>}
+            <div className='bottom-scroll'>
+                <div className='terminal-content-wrapper'>
+                    {
+                        loadTime
+                        ?
+                        <span className={'cursor blink'}/>
+                        :
+                        <>
                         {contentArray.map((e, i) => {
                             return <Content key={i} {...e}/>
                         })}
-                        <Editor {...current}/>
-                    </div>
+                        <Editor {...editor}/>
+                        </>
+                    }
                 </div>
-                </>
-            } 
+            </div> 
         </div>
     ) 
 };
+
 const Header = () => <div className='terminal-header'/>;
 
 const Content = ({user, host, dir, command, output}) => {
@@ -92,7 +157,7 @@ const Content = ({user, host, dir, command, output}) => {
                 </span>
             </span>
             <span>
-                    {output}
+                {output}
             </span>
         </div>
     )
@@ -117,14 +182,6 @@ const Editor = ({user, host, dir, typing, command}) => {
     )
 }
 
-const lsPortfolio = 
-    <span>
-        <div>about.txt</div>
-        <div>education.txt</div>
-        <div>skills.txt</div>
-        <div>projects/</div>
-    </span>;
-console.log(lsPortfolio);
 export default Terminal;
 
 
