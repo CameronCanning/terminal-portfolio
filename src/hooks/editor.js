9858c4b6ab416484cf9cd81b330227406eb10ecb
Cameron Canning <cameroncanning.1@gmail.com>
2022-05-03 20:07:35 -0400

    auto type takes a callback, start delay and end delay as arguments

--- src/hooks/editor.js
+++ src/hooks/editor.js
@@ -1,23 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-export const useAutoType = (speed) => {
+export const useAutoType = (speed=50, startDelay=1000, endDelay=1000) => {
     const [string, setString] = useState('');
     const [auto, setAuto] = useState('');
     const [typing, setTyping] = useState(false);
+    const [callback, setCallback] = useState(()=>()=>{});
+    const didMount = useRef(false);
 
     useEffect(() => {
-        if (auto) {
+        if (!didMount.current) {
+            didMount.current = true;
+        }
+        else if (auto) {
             setTimeout(() => {
                 setAuto(prev => prev.substring(1));
                 setString(prev => prev + auto[0]);
             }, speed)
         }
+        else if (typing) {
+            setTyping(false);
+            setTimeout(() => {
+                callback(string);
+                setCallback(()=>()=>{});
+            }, endDelay);
+        }
     }, [auto])
 
     return [
         `${string}`,
-        (autoString, delay) => {
-            setTimeout(() => setAuto(autoString), delay)
+        (autoString, callback, delay) => {         
+            setCallback((()=>callback));   
+            setTimeout(() => {
+                setAuto(autoString);
+                setTyping(true); 
+            }, delay || startDelay);
             
         },
         typing
